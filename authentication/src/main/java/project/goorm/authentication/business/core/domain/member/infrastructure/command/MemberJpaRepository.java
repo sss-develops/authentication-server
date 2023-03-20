1924a9658bddc276d3ab1fc23fc020de14c6f431
devjun10 <devjun1023@gmail.com>
2023-03-21 02:41:04 +0900

    build: JPA Congifuration 패키지 이동
    - Configuration 패키지 내부 한 곳에서 관리하기 위해 패키지 이동

--- authentication/src/main/java/project/goorm/authentication/business/core/domain/member/infrastructure/command/MemberJpaRepository.java
+++ authentication/src/main/java/project/goorm/authentication/business/core/domain/member/infrastructure/command/MemberJpaRepository.java
@@ -1,7 +1,7 @@
 package project.goorm.authentication.business.core.domain.member.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import project.goorm.authentication.business.core.domain.entity.Member;
+import project.goorm.authentication.business.core.domain.member.entity.Member;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
 }
