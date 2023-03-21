9baa2feb8190f20c3274ec722c8ded669c6a00a6
devjun10 <devjun1023@gmail.com>
2023-03-21 10:50:48 +0900

    feature: 로그인 API 개발
    - 한 사용자가 여러 기기에서 로그인 할 수 있도록 구현
     *같은 세션으로는 중복 로그인 불가

---
+++ authentication/src/main/java/project/goorm/authentication/business/web/client/redis/RedisSessionService.java
@@ -0,0 +1,14 @@
+package project.goorm.authentication.business.web.client.redis;
+
+import project.goorm.authentication.common.annotation.helper.ApplicationLayer;
+
+@ApplicationLayer
+public interface RedisSessionService {
+
+    String save(Long memberId);
+
+    Long getMemberId(String sessionId);
+
+    Long getLoginTryCount(Long memberId);
+
+}
---
+++ authentication/src/main/java/project/goorm/authentication/business/web/member/presentation/CookieUtils.java
@@ -0,0 +1,23 @@
+package project.goorm.authentication.business.web.member.presentation;
+
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtils {
+
+    private static final String SESSION_ID = "SESSION_ID";
+    private static final long FIFTEEN_MINUTE = 60 * 15;
+
+    public String createCookie(String session) {
+        return ResponseCookie.from(SESSION_ID, session)
+                .path("/")
+                .domain("SSS")
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("Lax")
+                .maxAge(FIFTEEN_MINUTE)
+                .build()
+                .toString();
+    }
+}
